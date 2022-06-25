998bc9956008a989eff029515055621c8072f36c
Vaibhav <vaibhavgoyalop@gmail.com>
2022-06-25 19:10:37 +0530

    on my way to fixing dijkstra'

--- GraphVisualiser/simulation.py
+++ GraphVisualiser/simulation.py
@@ -4,6 +4,8 @@ import pandas as pd
 import collections
 import dataframe_image as dfi
 import heapq
+from bokeh.io import export_png, export_svgs
+from bokeh.models import ColumnDataSource, DataTable, TableColumn
 
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 
@@ -372,3 +374,16 @@ def captureGraph(G, nodeColors, edgeColors, weighted):
 
 def captureDataFrame(df):
     dfi.export(df, f"static/output/data{PLOTNO}.png")
+    # save_df_as_image(df, f"static/output/data{PLOTNO}.png")
+
+
+def save_df_as_image(df, path):
+    source = ColumnDataSource(df)
+    df_columns = [df.index.name]
+    df_columns.extend(df.columns.values)
+    columns_for_table=[]
+    for column in df_columns:
+        columns_for_table.append(TableColumn(field=column, title=column))
+
+    data_table = DataTable(source=source, columns=columns_for_table,height_policy="auto",width_policy="auto",index_position=None)
+    export_png(data_table, filename = path)
