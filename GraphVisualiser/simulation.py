6791e0e3a4a4ea0af7c7fb9c4e4879ca373f3639
Vaibhav Goyal <89004991+vabugoyal@users.noreply.github.com>
2024-04-12 13:04:09 +0530

    update dfi parameters

--- GraphVisualiser/simulation.py
+++ GraphVisualiser/simulation.py
@@ -388,7 +388,7 @@ def captureGraph(G, nodeColors, edgeColors, weighted):
 
 def captureDataFrame(df):
     # print(df)
-    dfi.export(df, f"static/output/data{PLOTNO}.png")
+    dfi.export(df, f"static/output/data{PLOTNO}.png", table_conversion='matplotlib')
     # save_df_as_image(df, f"static/output/data{PLOTNO}.png")
 
 
